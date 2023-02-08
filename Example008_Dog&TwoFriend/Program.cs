477f1b75f8f6fcd9792ba57d7c5000d214ed9fdc
Sergey <gul.19967@gmail.com>
2023-02-09 01:33:57 +0300

    add Example 008 Dog & Two Friend

---
+++ Example008_Dog&TwoFriend/Program.cs
@@ -0,0 +1,26 @@
+﻿int count, distance, first_friend_speed, second_friend_speed, dog_speed, friend, time;
+
+count = 0;
+distance = 10000;
+first_friend_speed = 1;
+second_friend_speed = 2;
+dog_speed = 5;
+friend = 2;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = (int) ((double) distance / (first_friend_speed + dog_speed));
+        friend = 2;
+    }
+    else
+    {
+        time = (int) ((double) distance / (second_friend_speed + dog_speed));
+        friend = 1;
+    }
+
+    distance = distance - (first_friend_speed + second_friend_speed) * time;
+    count++;
+}
+    Console.WriteLine(count);
