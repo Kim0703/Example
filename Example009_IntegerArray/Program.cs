ff70343ef23ece67f12fa4f7b3b9a8e672c7785d
Sergey <gul.19967@gmail.com>
2023-02-12 19:35:16 +0300

    commit

--- Example009_IntegerArray/Program.cs
+++ Example009_IntegerArray/Program.cs
@@ -1,34 +1,17 @@
-﻿using System;
-
-public class MyProgram
-{
-    public static void Main(string[] args)
-    {
-        int index, max, lenght;
-        int[] n = new int[5];
+﻿int index, max, lenght;
+int[] n = new int[5];
         
-        lenght = n.Length;
-        index = 0;
-        max = 0;
-        while (index < lenght)
-        {
-            Console.WriteLine("Введите ваше число: ");
-            n[index] = (int) inputValue();
-            if (n[index] > max)
-            {
-                max = n[index];
-            }
-            index = index + 1;
-        }
-        Console.WriteLine(max);
-    }
-    
-    // .NET can only read single characters or entire lines from the
-    // console. The following function safely reads a double value.
-    private static double inputValue()
+lenght = n.Length;
+index = 0;
+max = 0;
+while (index < lenght)
+{
+    Console.WriteLine("Введите ваше число: ");
+    n[index] = Convert.ToInt32(Console.ReadLine());
+    if (n[index] > max)
     {
-        double result;
-        while (!double.TryParse(Console.ReadLine(), out result));
-        return result;
+        max = n[index];
     }
+    index++;
 }
+Console.WriteLine(max);
